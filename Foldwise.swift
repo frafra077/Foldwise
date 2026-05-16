6aac259712e66ac5700f366768e50ecdcf5a4e1f
Francesco <francescoalg@gmail.com>
2026-05-16 15:35:42 +0200

    Create Foldwise.swift

---
+++ Foldwise.swift
@@ -0,0 +1,10 @@
+// Foldwise
+// Copyright © 2026 Foldwise. All rights reserved.
+
+import Foundation
+
+enum AppVersion {
+    static let current = "1.0.0"
+    static let build = "1"
+    static let minimumMacOS = "14.0"
+}
